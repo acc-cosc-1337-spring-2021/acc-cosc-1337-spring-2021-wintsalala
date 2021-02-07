51e5be68ce5cb2ab650134da99d9feb14325153b
wintsalala <wintsalala@gmail.com>
2021-02-06 23:52:08 -0600

    Winta Fuji - Homework 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,21 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
+#include <iostream>
 
-
+using std:: cin;
+using std:: cout;
+using std:: endl;
 
 int main() 
 {
-
+	int num;
+	cin >> num;
+	int result = multiply_numbers(num);
+	cout << result << endl;
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout << result << endl;
 	return 0;
 }
 
