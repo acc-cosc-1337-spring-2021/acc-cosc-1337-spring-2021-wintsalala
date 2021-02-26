e36e337063a864979dcd10d65acd55c3500ff44c
wintsalala <wintsalala@gmail.com>
2021-02-25 21:44:29 -0600

    Winta Fuji Homework03

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,57 @@
 //cpp
+#include<string>
+using std:: string;
+
+string get_letter_grade_using_if(int grade)
+{
+    string letter_grade;
+    if(grade >= 90 && grade <= 100)
+    {
+        letter_grade = "A";
+    }
+    else if(grade >= 80 && grade <= 89)
+    {
+       letter_grade = "B";
+    }
+    else if(grade >= 70 && grade <= 79)
+    {
+       letter_grade = "C";
+    }
+    else if(grade >= 60 && grade <= 69)
+    {
+       letter_grade = "D";
+    }
+    else if(grade >= 0 && grade <= 59)
+    {
+       letter_grade = "F";
+    }
+
+    return letter_grade;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+     string letter_grade;
+    switch (grade / 10)
+    {
+        case 10:
+        case 9:
+            letter_grade = "A";
+            break;
+        case 8:
+            letter_grade = "B";
+            break;
+        case 7:
+            letter_grade = "C";
+            break;
+        case 6:
+            letter_grade = "D";
+            break;
+        
+        default:
+            letter_grade = "F";
+            break;
+    }
+
+    return letter_grade;
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,29 @@
 //Write the include statement for decisions.h here
+#include <string>
+#include <iostream>
+#include "decision.h"
+
+
+using std:: string;
+using std:: cin;
+using std:: cout;
 
 //Write namespace using statements for cout and cin
 
 int main() 
 {
-
+	int grade;
+	cout << "Enter your grade number: ";
+	cin >> grade;
+	if (grade >= 0 && grade <= 100)
+	{
+		cout<<"Your grade(using if/else) is : " << get_letter_grade_using_if(grade);
+		cout<<"\nYour grade(using switch) is: " << get_letter_grade_using_switch(grade);
+	}
+	else
+	{
+		cout<<"Number out of range";
+	}
+	
 	return 0;
 }
-
